4c49b9f7ba60de20af08614bcf9ff9a0e0c66e6b
not-a-hack-er <akarshbajpai99@gmail.com>
2025-09-20 10:17:05 +0530

    Time: 2 ms (13.27%) | Memory: 9 MB (27.24%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string k) {
+        stack<char> ans;
+      for(int i=0;i<k.size();i++)
+      {
+          char ch=k[i];
+     if(ch=='{'||ch=='('||ch=='[')
+          ans.push(ch);
+          
+      else
+      {
+          if(!ans.empty())
+          {
+              char top=ans.top();
+              if((ch=='}')&&(top=='{')||(ch==')')&&(top=='(')||(ch==']')&&(top=='['))
+              {
+              ans.pop();
+              }
+              else{
+                  return false;
+              }
+          }
+          
+          else
+          {
+            return false;
+            }
+       }
+          
+      }
+      if(ans.empty())
+      return true;
+      else
+      return false;
+    }
+};
