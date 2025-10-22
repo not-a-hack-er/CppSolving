de172ebd5c578252e78222064c1134636870aa94
not-a-hack-er <akarshbajpai99@gmail.com>
2025-10-22 18:36:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (22.92%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
-void markRow(vector<vector<int>>& matrix, int n,int m, int i)
-{
-    for(int j=0;j<m;j++)
-    {
-        if(matrix[i][j]!=0)
-        {
-            matrix[i][j]=INT_MIN + 69;
-        }
-    }
-}
-void markCol(vector<vector<int>>& matrix,int n,int m,int j)
-{
-    for(int i=0;i<n;i++)
-    {
-        if(matrix[i][j]!=0)
-        {
-            matrix[i][j]=INT_MIN + 69;
-        }
-    }
-}
-    void setZeroes(vector<vector<int>>& matrix) 
-    {
-        int n = matrix.size();
-    int m = matrix[0].size();
-        for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == 0) {
-                markRow(matrix, n, m, i);
-                markCol(matrix, n, m, j);
-            }
-        }
-    }
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == INT_MIN + 69) {
-                matrix[i][j] = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
             }
         }
-    }
-        
-        
+         for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(row[i]||col[j])
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
     }
 };
