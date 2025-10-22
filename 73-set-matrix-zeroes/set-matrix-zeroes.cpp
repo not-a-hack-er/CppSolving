50136e0162a8b1dbd64a3805a24a8bc49688427f
not-a-hack-er <akarshbajpai99@gmail.com>
2025-10-22 18:08:33 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (51.85%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+void markRow(vector<vector<int>>& matrix, int n,int m, int i)
+{
+    for(int j=0;j<m;j++)
+    {
+        if(matrix[i][j]!=0)
+        {
+            matrix[i][j]=INT_MIN + 69;
+        }
+    }
+}
+void markCol(vector<vector<int>>& matrix,int n,int m,int j)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(matrix[i][j]!=0)
+        {
+            matrix[i][j]=INT_MIN + 69;
+        }
+    }
+}
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+        int n = matrix.size();
+    int m = matrix[0].size();
+        for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                markRow(matrix, n, m, i);
+                markCol(matrix, n, m, j);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == INT_MIN + 69) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+        
+        
+    }
+};
