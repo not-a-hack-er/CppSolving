ddf1eb92a911bc663b117233d3714aff72869cc1
not-a-hack-er <akarshbajpai99@gmail.com>
2025-10-25 01:14:22 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (44.20%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+     int height(struct TreeNode* node){
+        //base case
+        if(node == NULL) {
+            return 0;
+        }
+        
+        int left = height(node->left);
+        int right = height(node->right);
+        
+        int ans = max(left, right) + 1;
+        return ans;
+    }
+     pair<int,int> diameterFast(TreeNode* root) {
+        //base case
+        if(root == NULL) {
+            pair<int,int> p = make_pair(0,0);
+            return p;
+        }
+        
+        pair<int,int> left = diameterFast(root->left);
+        pair<int,int> right = diameterFast(root->right);
+        
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second + 1;
+        
+        pair<int,int> ans;
+        ans.first = max(op1, max(op2, op3));;
+        ans.second = max(left.second , right.second) + 1;
+
+        return ans;
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        return diameterFast(root).first-1;
+    }
+};
