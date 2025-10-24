d5883f4e4da7ca9134d6e3f27819fc6d3eb8fa54
not-a-hack-er <akarshbajpai99@gmail.com>
2025-10-25 02:01:28 +0530

    Time: 2 ms (77.58%) | Memory: 7.8 MB (64.07%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a =0;
+        int b = 1;
+        int c =0;
+        for(int i=1;i<=n;i++) {
+            a=b;
+            b=c;
+            c=a+b;
+        }
+        return c;
+    }
+};
