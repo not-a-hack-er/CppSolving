132c12e070cbd087b5feab50e174a6ba3c7b4f6e
Akarsh bajpai <akarshbajpai99@gmail.com>
2025-11-24 23:35:42 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (79.86%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int result = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+        if (i % 2 == 0)
+            result += nums[i];
+        else
+            result -= nums[i];
+    }
+    return result;
+    }
+};
