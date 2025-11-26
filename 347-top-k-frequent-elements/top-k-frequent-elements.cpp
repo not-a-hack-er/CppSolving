ce437ce3c5e923b2a564004286b21556c1ed54a8
Akarsh bajpai <akarshbajpai99@gmail.com>
2025-11-26 22:57:28 +0530

    Time: 2 ms (57.04%) | Memory: 17.8 MB (83.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-unordered_map<int, int> ab;
-for(int i = 0; i < nums.size(); i++) {
-    ab[nums[i]]++;       // Count frequencies
-}
-priority_queue<pair<int,int>> heap;
-for(auto& p : ab) {
-    heap.push({p.second, p.first});  // Push {frequency, value}
-}
-vector<int> ans;
-for(int i = 0; i < k && !heap.empty(); i++) {
-    ans.push_back(heap.top().second);  // Get most frequent value
-    heap.pop();
-}
-return ans;
+  unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+        
+        // Make a vector of pairs: {number, frequency}
+        vector<pair<int, int>> freqList;
+        for (auto it : freq) {
+            freqList.push_back({it.first, it.second});
+        }
+        
+        // Sort the vector by frequency, highest first
+        sort(freqList.begin(), freqList.end(),
+             [](pair<int,int> a, pair<int,int> b) {
+                 return a.second > b.second;
+             });
+        
+        // Get the first k elements
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
+            ans.push_back(freqList[i].first);
+        }
+        return ans;
 
 
 
