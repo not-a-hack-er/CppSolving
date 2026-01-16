2e2b4cc9539196a77cd9c4e7aaad6456e4645ce6
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-01-16 19:34:52 +0530

    Time: 12 ms (22.40%) | Memory: 104.5 MB (21.23%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,23 +1,41 @@
 class Solution {
+
 public:
+    int dfs(vector<vector<int>>& grid, int r, int c, vector<vector<bool>>& visited) {
+        if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size()) {
+            return 1;
+        }
+        
+        if (grid[r][c] == 0) {
+            return 1;
+        }
+        if (visited[r][c]) {
+            return 0;
+        }
+        visited[r][c] = true;
+        int perimeter = 0;
+        perimeter += dfs(grid, r + 1, c, visited);
+        perimeter += dfs(grid, r - 1, c, visited);
+        perimeter += dfs(grid, r, c + 1, visited);
+        perimeter += dfs(grid, r, c - 1, visited);
+        
+        return perimeter;
+    }
+
     int islandPerimeter(vector<vector<int>>& grid) {
-        if(grid.size()==0)
-        return 0;
-        int per=0;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[0].size();j++)
-            {
-                if(grid[i][j]==1)
-                {
-                per+=4;
-                if((i>0 && grid[i-1][j]==1))
-                per-=2;
-                if(j>0 && grid[i][j-1]==1)
-                per-=2;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+    
+                if (grid[i][j] == 1) {
+                    return dfs(grid, i, j, visited);
                 }
             }
         }
-        return per;
+        return 0;
+    
     }
 };
