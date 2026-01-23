c7ec3e8b3b34ffa284b17b514ea53e8665456492
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-01-24 00:34:41 +0530

    Time: 8 ms (16.91%) | Memory: 27.2 MB (34.14%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -1,48 +1,35 @@
 class Solution {
 public:
-void shiftLeft(vector<int>& nums,int idx)
-{
-    int n=nums.size();
-    for(int i=idx;i<n-1;i++)
-    {
-        nums[i]=nums[i+1];
-    }
-    nums.pop_back();
-}
-void mergePairs(vector<int>& nums)
-{
-    int n=nums.size();
-    int min=INT_MAX,minidx=-1;
-    for(int i=0;i<n-1;i++)
-    {
-        int sum=nums[i]+nums[i+1];
-        if(sum<min)
-        {
-            min=sum;
-            minidx=i;
-        }
-    }
-    nums[minidx]=min;
-    shiftLeft(nums,minidx+1);
-}
-bool isSorted(vector<int>& nums)
-{
-    int n=nums.size();
-    for(int i=0;i<n-1;i++)
-    {
-        if(nums[i+1]<nums[i])
-        return false;
-    }
-    return true;
-}
-
     int minimumPairRemoval(vector<int>& nums) {
-        int result=0;int n=nums.size();
-        while(!isSorted(nums))
-        {
-            mergePairs(nums);
-            result++;
+         vector<int> list(nums.begin(), nums.end());
+
+        if (isSorted(list)) return 0;
+
+        int cnt = 0;
+        while (!isSorted(list)) {
+            int sum = INT_MAX;
+            int idx = -1;
+
+            for (int i = 0; i < (int)list.size() - 1; i++) {
+                int currSum = list[i] + list[i + 1];
+                if (currSum < sum) {
+                    sum = currSum;
+                    idx = i;
+                }
+            }
+
+            int merged = list[idx] + list[idx + 1];
+            list.erase(list.begin() + idx + 1);
+            list[idx] = merged;
+            cnt++;
         }
-        return result;
+        return cnt;
+    }
+
+    bool isSorted(const vector<int>& list) {
+        for (int i = 0; i < (int)list.size() - 1; i++) {
+            if (list[i] > list[i + 1]) return false;
+        }
+        return true;
     }
 };
