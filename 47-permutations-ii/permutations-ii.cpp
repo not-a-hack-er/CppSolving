7105f7ac521f3143437c5ba1e139ede6c0d09905
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-02-04 19:15:35 +0530

    Time: 3 ms (64.72%) | Memory: 12.3 MB (45.51%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+         int n;
+        vector<vector<int>> result;
+        void solve(unordered_map<int,int> &mp,vector<int> &temp)
+        {
+            for(auto& [num,count]:mp)
+            {
+                if(temp.size()==n)
+                {
+                    result.push_back(temp);
+                    return;
+                }
+                if(count==0)
+                continue;
+                temp.push_back(num);
+                mp[num]--;
+                solve(mp,temp);
+                temp.pop_back();
+                mp[num]++;
+            }
+        }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+       n=nums.size();
+       unordered_map<int,int> mp;
+       for(int &num:nums)
+       {
+        mp[num]++;
+       }
+       vector<int> temp;
+       solve(mp,temp);
+       return result;
+
+    }
+};
