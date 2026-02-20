09b62c0e474592adbb51a77b4d0e9e2ac0138b70
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-02-20 23:27:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (10.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        vector<int> count(26,0);
+        for(char ch:s)
+        {
+            count[ch-'a']++;
+        }
+        for(char ch:t)
+        {
+            count[ch-'a']--;
+        }
+        bool allZeroes=all_of(begin(count),end(count),[](int element){
+            return element==0;
+        });
 
-        return s==t;
-
+        return allZeroes;
     }
 };
