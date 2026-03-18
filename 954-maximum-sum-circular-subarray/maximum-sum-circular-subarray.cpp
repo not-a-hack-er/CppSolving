3969ef496217026da49d372e8e69d7e6ff38af7d
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-03-18 23:22:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (49.06%) - LeetSync

---
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int total = 0;
+
+        int maxSum = nums[0], currMax = 0;
+        int minSum = nums[0], currMin = 0;
+
+        for(int x : nums) {
+            currMax = max(x, currMax + x);
+            maxSum = max(maxSum, currMax);
+
+            currMin = min(x, currMin + x);
+            minSum = min(minSum, currMin);
+
+            total += x;
+        }
+
+       
+        if(maxSum < 0) return maxSum;
+
+        return max(maxSum, total - minSum);
+    }
+};
