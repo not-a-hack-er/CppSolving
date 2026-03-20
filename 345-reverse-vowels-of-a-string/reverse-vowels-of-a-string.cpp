62c23ce02a534df4f1a463964632b196be92ef76
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-03-20 12:09:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (51.15%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    bool isVowel(char c) {
-        return (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
-                c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
-    }
-
     string reverseVowels(string s) {
-        int st = 0, l = s.size() - 1;
-
-        while(st < l) {
-            if(isVowel(s[st]) && isVowel(s[l])) {
-                swap(s[st], s[l]);
-                st++;
-                l--;
+        int st=0,l=s.size()-1;
+        while(st<l)
+        {
+            if((s[st]=='a'||s[st]=='e'||s[st]=='i'||s[st]=='o'||s[st]=='u'||s[st]=='A'||s[st]=='E'||s[st]=='I'||s[st]=='O'||s[st]=='U') && (s[l]=='a'||s[l]=='e'||s[l]=='i'||s[l]=='o'||s[l]=='u'||s[l]=='A'||s[l]=='E'||s[l]=='I'||s[l]=='O'||s[l]=='U'))
+            {
+            swap(s[st],s[l]);
+            st++;
+            l--;
             }
-            else if(!isVowel(s[st])) {
-                st++;
+            else if(s[st]!='a'&&s[st]!='e'&&s[st]!='i'&&s[st]!='o'&&s[st]!='u'&&s[st]!='A'&&s[st]!='E'&&s[st]!='I'&&s[st]!='O'&&s[st]!='U')
+            {
+            st++;
             }
-            else {
-                l--;
-            }
+           else
+            l--;
         }
-
         return s;
+        
     }
 };
