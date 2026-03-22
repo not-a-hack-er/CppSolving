8739436fb5551fd04a16b6cf82138a8b2b0d8a73
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-03-22 11:33:25 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (12.49%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -3,14 +3,10 @@ public:
     bool isPowerOfTwo(int n) {
         if(n==1)
         return true;
-        long long si=2;
-        while(si<n)
-        {
-            si*=2;
-        }
-        if(si==n)
-        return true;
-        else
+        if(n<=0||n%2!=0)
         return false;
+        return isPowerOfTwo( n/2);
+    
+    
     }
 };
