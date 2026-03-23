b1bfa183ecd903cc4a24fb232306ac8d3729774e
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-03-23 11:11:30 +0530

    Time: 3 ms (52.19%) | Memory: 13.8 MB (99.58%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
- set<vector<int>> s; 
+    void solve(vector<int>& arr, int target, int idx,
+               vector<int>& temp, vector<vector<int>>& ans) {
 
-void getAllCombinations(vector<int> &arr, int idx ,int target ,vector<vector<int>> &ans,vector<int> &combin)
-{
-    
-    if(idx==arr.size() || target<0)
-    {
-        return;
-    }
-    if(target==0)
-    {
-        if(s.find(combin)==s.end())
-        {
-            ans.push_back({combin});
-            s.insert(combin);
-          
-        }
-        return;
-    }
-        combin.push_back(arr[idx]);
-        getAllCombinations(arr,idx+1 ,target-arr[idx] ,ans,combin) ;
-
-        getAllCombinations(arr,idx ,target-arr[idx] ,ans,combin) ;
-
-        combin.pop_back();
-        getAllCombinations(arr,idx+1 ,target ,ans,combin) ;
-
-
-
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-       vector<vector<int>> ans;
-       vector<int> combin;
        
-     getAllCombinations(arr,0 ,target ,ans,combin);
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (idx >= arr.size() || target < 0) return;
+
+      
+        temp.push_back(arr[idx]);
+        solve(arr, target - arr[idx], idx, temp, ans);
+        temp.pop_back();
+
+       
+        solve(arr, target, idx + 1, temp, ans);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        solve(candidates, target, 0, temp, ans);
         return ans;
     }
 };
