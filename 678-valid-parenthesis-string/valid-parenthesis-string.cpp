f83d7393c3df3a24992a0ad45307cd96b2205352
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-03-24 09:28:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (20.35%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> st1, st2;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                st1.push(i);
+            }
+            else if (s[i] == '*') {
+                st2.push(i);
+            }
+            else {
+                if (!st1.empty()) st1.pop();
+                else if (!st2.empty()) st2.pop();
+                else return false;
+            }
+        }
+
+        
+        while (!st1.empty() && !st2.empty()) {
+            if (st1.top() < st2.top()) {
+                st1.pop();
+                st2.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return st1.empty();
+    }
+};
