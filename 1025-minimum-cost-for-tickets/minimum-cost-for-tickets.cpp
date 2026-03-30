ba2ee759a47c5261c3c07c5a09cd20466be4fc9d
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-03-30 19:53:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (41.32%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-int solve(int n,vector<int>& days, vector<int>& costs,int i,vector<int>& dp)
+
+int solve2(int n, vector<int>& days, vector<int>& costs)
 {
-    if(i>=n)
-    return 0;
-    if(dp[i]!=-1)
-    return dp[i];
-    int op1 = solve(n, days, costs, i+1,dp)+costs[0];
-    int k;
-    for(k=i;k<n && days[k]<days[i]+7;k++);
-    int op2=solve(n,days,costs,k,dp)+costs[1];
-    for(k=i;k<n && days[k]<days[i]+30;k++);
-    int op3=solve(n,days,costs,k,dp)+costs[2];
-    dp[i]=min(op1,min(op2,op3));
-    return dp[i];
+    vector<int> dp(n+1, 0);
+
+    for(int j = n-1; j >= 0; j--)
+    {
+        int op1 = dp[j+1] + costs[0];
+
+        int k;
+        for(k = j; k < n && days[k] < days[j] + 7; k++);
+        int op2 = dp[k] + costs[1];
+
+        for(k = j; k < n && days[k] < days[j] + 30; k++);
+        int op3 = dp[k] + costs[2];
+
+        dp[j] = min(op1, min(op2, op3));
+    }
+    return dp[0];
 }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
        int n=days.size();
-       vector<int> dp(n+1,-1);
-       return solve(n,days,costs,0,dp); 
+       //vector<int> dp(n+1,-1);
+       return solve2(n,days,costs); 
     }
 };
