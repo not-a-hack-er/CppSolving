d34bd1f674f801b73b15698a879f8180b13e4d87
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-03-30 21:42:11 +0530

    Time: 4 ms (87.09%) | Memory: 30.9 MB (62.69%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-int solve(vector<vector<char>> &mat, int i, int j, int &maxi, int &n, int &m, vector<vector<int>>& dp) {
-        if (i >= n || j >= m) {
-            return 0;
-        } 
-        if(dp[i][j]!=-1)
-        return dp[i][j];
+int solve(vector<vector<char>> &mat, int i, int j, int &maxi, int &n, int &m) {
+        vector<vector<int>> dp(n+1, vector<int>(m+1 ,0));
         
-        int right = solve(mat, i, j+1, maxi, n, m,dp);
-        int diagonal = solve(mat, i+1, j+1, maxi, n, m,dp);
-        int down = solve(mat, i+1, j, maxi, n, m,dp);
-        
-        if (mat[i][j] == '1') {
-            dp[i][j] =  1+min(right, min(diagonal, down));
-            
-            maxi = max(maxi, dp[i][j]);
-            
-            return dp[i][j];
+        for (int i=n-1; i>=0; i--) {
+            for (int j=m-1; j>=0; j--) {
+                int right = dp[i][j+1];
+                int diagonal = dp[i+1][j+1];
+                int down = dp[i+1][j];
+                
+                if (mat[i][j] == '1') {
+                    dp[i][j] = 1 + min(right, min(diagonal, down));
+                    maxi = max(maxi, dp[i][j]);
+                }
+                else {
+                    dp[i][j] = 0;
+                }
+            }
         }
-        else {
-            return dp[i][j]=0;
+        return dp[0][0];
         }
-    }
+    
     int maximalSquare(vector<vector<char>>& mat) {
         int maxi = 0;
         int n=mat.size();int m=mat[0].size();
-        vector<vector<int>> dp(n, vector<int>(m ,-1));
-        solve(mat, 0, 0, maxi, n, m,dp);
+        
+        solve(mat, 0, 0, maxi, n, m);
         return maxi*maxi;
     }
 };
