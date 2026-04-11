5d5efa4ee5d334c377e98aa8f71e633268c00d43
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-04-11 10:08:57 +0530

    Time: 27 ms (57.09%) | Memory: 27.3 MB (66.74%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+int solve(string text1, string text2,int i,int j,vector<vector<int>>& dp)
+{
+    int ans=0;
+    if(i==text1.size())
+        return 0;
+    if(j==text2.size())
+        return 0;
+    if(dp[i][j]!=-1)
+        return dp[i][j];
+    if(text1[i]==text2[j])
+         ans=1+solve(text1,text2,i+1,j+1,dp);
+    else
+    {
+        ans=max(solve(text1,text2,i+1,j,dp),solve(text1,text2,i,j+1,dp));
+    }
+    return dp[i][j]=ans;
+}
+int solveTab(string a,string b)
+{
+    vector<vector<int>> dp(a.length()+1,vector<int>(b.length()+1));
+    for(int i=a.size()-1;i>=0;i--)
+    {
+        for(int j=b.size()-1;j>=0;j--)
+        {
+            int ans=0;
+            if(a[i]==b[j])
+            {
+                ans=1+dp[i+1][j+1];
+            }
+            else
+            {
+                ans=max(dp[i+1][j],dp[i][j+1]);
+            }
+            dp[i][j]=ans;
+        }
+    }
+    return dp[0][0];
+}
+    int longestCommonSubsequence(string text1, string text2) {
+        //vector<vector<int>> dp(text1.size(),vector<int> (text2.size(),-1));
+        return solveTab(text1,text2);
+    }
+};
