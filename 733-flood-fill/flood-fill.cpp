293fe2146149f34683bed6c7b70daae99eb9170b
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-06-09 19:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (75.74%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,35 +1,29 @@
 class Solution {
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m=image.size();
-        int n=image[0].size();
-
-
-        int prev=image[sr][sc];
-     if (prev == color)
-      return image;
-
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        image[sr][sc]=color;
-        int x[4]={1,0,-1,0};
-        int y[4]={0,1,0,-1};
-        while(!q.empty())
+    public:
+    void dfs(vector<vector<int>>& ans,vector<vector<int>>& image,int row, int col, int color,int delRow[],int delCol[],int ini)
+    {
+        ans[row][col] = color;
+        int n=image.size();
+        int m=image[0].size();
+        for(int i=0;i<4;i++)
         {
-            auto[nx,ny]=q.front();
-            q.pop();
-
-            for(int i=0;i<4;i++)
+            int nrow = row+delRow[i];
+            int ncol = col+delCol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==ini && ans[nrow][ncol]!=color)
             {
-                int cx = nx + x[i];
-                int cy = ny + y[i];
-                if (cx >= 0 && cy >= 0 && cx < m && cy < n && image[cx][cy]==prev)
-                {
-                    image[cx][cy]=color;
-                    q.push({cx,cy});
-                }
+               dfs(ans,image,nrow,ncol,color,delRow,delCol,ini);
             }
         }
-        return image;
+    }
+public:
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>> ans= image;
+        int ini=image[sr][sc];
+        int delRow[]={-1,0,+1,0};
+        int delCol[]={0,+1,0,-1};
+        dfs(ans,image,sr,sc,color,delRow,delCol,ini);
+
+        return ans;
     }
 };
