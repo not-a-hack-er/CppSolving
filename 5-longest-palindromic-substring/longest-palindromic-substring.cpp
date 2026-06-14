4cfa74c04fa92808add19851e614d2018e040e21
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-06-14 11:07:21 +0530

    Time: 7 ms (87.14%) | Memory: 9.5 MB (64.86%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    string longestPalindrome(string s) {
+
+        int n = s.size();
+        int start = 0, len = 1;
+
+        for(int i = 0; i < n; i++) {
+
+            // Odd length palindrome
+            int l = i, r = i;
+
+            while(l >= 0 && r < n && s[l] == s[r]) {
+
+                if(r - l + 1 > len) {
+                    len = r - l + 1;
+                    start = l;
+                }
+
+                l--;
+                r++;
+            }
+
+            // Even length palindrome
+            l = i;
+            r = i + 1;
+
+            while(l >= 0 && r < n && s[l] == s[r]) {
+
+                if(r - l + 1 > len) {
+                    len = r - l + 1;
+                    start = l;
+                }
+
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(start, len);
+    }
+};
