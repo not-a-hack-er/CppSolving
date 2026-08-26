41260c39f0e0046b019a5ef2e8e2e21072007d27
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-08-27 01:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (82.61%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='1')
+            continue;
+            int c=0;
+            for(int j=i;j<s.size();j++){
+                if(s[j]=='1')
+                    c++;
+                if(c==k)
+                {
+                    string temp=s.substr(i,j-i+1);
+                    if(ans==""||temp.size()<ans.size()|| (temp.size()==ans.size() && temp<ans))
+                    ans=temp;
+                     break;
+                }
+                //break;
+            }
+        }  return ans;
+    }
+  
+};
