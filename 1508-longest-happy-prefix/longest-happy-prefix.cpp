04a6f3ac1cdbd814b9e6398d302d6520e5f37cc4
Akarsh bajpai <akarshbajpai99@gmail.com>
2026-09-11 02:23:33 +0530

    Time: 16 ms (12.30%) | Memory: 23.5 MB (18.53%) - LeetSync

---
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n=s.size();
+        int i=1,len=0;
+        vector<int> lps(n,0);
+        while(i<n){
+            if(s[i]==s[len]){
+                len++;
+                lps[i]=len;
+                i+=1;
+            }else{
+                if(len!=0){
+                    len=lps[len-1];
+                }else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        string ans="";
+        ans=s.substr(0,len);
+        return ans;
+    }
+};
